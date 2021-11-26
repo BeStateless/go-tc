dcbaa12f62c4ef5730074fb6695a39b4a089eced
Dave Setzke <david.setzke@stateless.net>
2021-11-26 06:34:40 -0700

    fixup merge errors

--- example_flower_test.go
+++ example_flower_test.go
@@ -5,12 +5,12 @@ package tc_test
 
 import (
 	"fmt"
-	"github.com/bestateless/go-tc"
-	"github.com/bestateless/go-tc/core"
-	"github.com/bestateless/go-tc/internal/unix"
 	"net"
 	"os"
 
+	"github.com/bestateless/go-tc"
+	"github.com/bestateless/go-tc/core"
+	"github.com/bestateless/go-tc/internal/unix"
 	"github.com/jsimonetti/rtnetlink"
 )
 
